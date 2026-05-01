6a9400c15937890e282bd2c8ba39f9c3160d3e23
SpaceLoverEvee <spaceenthusiast.aerospace@gmail.com>
2026-05-02 00:02:31 +0530

    Add files via upload

--- SimMain.py
+++ SimMain.py
@@ -1,16 +1,49 @@
 import numpy as np
 
-x = 0.5
-y = 0.5
-z = 0
+
+TotalForce = np.array([0, 0, 0])
+TotalTorque = np.array([0, 0, 0])
+
 
 I0 = 1.5
 w0 = 1.0
 
-r = np.sqrt(x**2 + y**2)
 
-I = I0 * np.exp(-2 * r**2 / w0**2)
+class RectangleLightSail():
+    def __init__(self, width, height, StartingX, StartingY):
+        
+        self.width = width * 100
+        self.height = height * 100
+        self.StartingX = StartingX
+        self.StartingY = StartingY
 
-position = np.array([x, y, z])
-print("Position: ", position)
-print("Intensity: ", I)
+
+    def compute(self):
+        x = self.StartingX
+        y = self.StartingY
+        
+        Force = np.array([0, 0, 0])
+        Torque = np.array([0, 0, 0])
+        
+        for i in range(self.width):
+            for j in range(self.height):
+                r = np.sqrt(x**2 + y**2)
+                I = I0 * np.exp(-2 * r**2 / w0**2)
+                position = np.array([x, y, 0])
+                
+                normal = np.array([0, 0, 1])
+                F = I * normal
+                
+                tau = np.cross(position, F)
+                
+                Force = Force + F
+                Torque = Torque + tau
+                
+                y += 1
+            x += 1
+        return Force, Torque
+
+nyx1 = RectangleLightSail(10, 10, -5, -5)
+TotalForce, TotalTorque = nyx1.compute()
+
+print(f"total force is {TotalForce} \n total torque is {TotalTorque}")
